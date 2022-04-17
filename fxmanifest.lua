bee1b8f7296a8011736b632824921e10db8a17e1
Boz <ryanmturnbull@gmail.com>
2022-04-17 18:21:43 +1000

    Removed SSA

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,25 +11,19 @@ server_script 'server.lua'
 
 files {
 	-- Weeby Siren Fixes
-	"data/simplesound_test_sounds.dat54.rel",
-	"data/wsound_game.dat151.rel",
-	"data/wsound_sounds.dat54.rel",
+	--"data/simplesound_test_sounds.dat54.rel",
+	--"data/wsound_game.dat151.rel",
+	--"data/wsound_sounds.dat54.rel",
 
 	-- Addon Sounds
 	"dlc_serversideaudio/oiss_ssa_vehaud_etc.awc",
-	"dlc_serversideaudio/oiss_ssa_vehaud_etc2.awc",
-	"dlc_serversideaudio/oiss_ssa_vehaud_etc3.awc",
-	"dlc_serversideaudio/oiss_ssa_vehaud_etc4.awc",
-	"dlc_serversideaudio/oiss_ssa_vehaud_etc5.awc",
-	"dlc_serversideaudio/oiss_ssa_vehaud_etc6.awc",
-	"data/serversideaudio_sounds.dat54.nametable",
 	"data/serversideaudio_sounds.dat54.rel"
 }
 
 -- Weeby Siren Fixes
-data_file 'AUDIO_GAMEDATA' 'data/wsound_game.dat'
-data_file "AUDIO_SOUNDDATA" "data/simplesound_test_sounds.dat"
-data_file "AUDIO_SOUNDDATA" "data/wsound_sounds.dat"
+--data_file 'AUDIO_GAMEDATA' 'data/wsound_game.dat'
+--data_file "AUDIO_SOUNDDATA" "data/simplesound_test_sounds.dat"
+--data_file "AUDIO_SOUNDDATA" "data/wsound_sounds.dat"
 
 -- Addon Sounds
 data_file "AUDIO_WAVEPACK" "dlc_serversideaudio"
