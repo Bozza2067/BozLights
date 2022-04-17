787380590d72eadaf3ae1770535bce466777542f
Boz <ryanmturnbull@gmail.com>
2022-04-17 11:17:51 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,15 +4,12 @@ UsingVanillaSiren = false
 -- Vehicles with no third tone: Useful for california-compliant sirens that only have Wail and Yelp.
 ModelsWithNoTertiaryTone = 
 {
-    "POLICE",
-    "POLICE2",
-    "POLICE4",
-    "SHERIFF",
+    "bcsonalamo",
 }
 -- Vehicles with a 4th tone activated by a seperate button. Usually a Hi-Lo.
 ModelsWithQuarternaryTone = 
 {
-	"POLICE3",
+    "bcsonalamo"
 }
 -- Vehicles with dual sirens
 ModelsWithAuxiliaryTone = 
@@ -22,25 +19,18 @@ ModelsWithAuxiliaryTone =
 -- Vehicles that use the horn to change tones. Disabled electronic horn. Horn behavior: Regular car horn when lights off, manual tone when lights on, change tone when sirens on.
 ModelsWithHornSwitch = 
 {
-    "POLICE",
-    "POLICE2",
-    "POLICE4",
+    " bcsonalamo",
 }
 
 
 
 ModelsWithSS2000 =
 {
-    "POLICE",
-    "POLICE2",
-    "POLICE4",
-    "SHERIFF",
-    "SHERIFF2",
 }
 
 ModelsWithSSP3000 =
 {
-	"POLICE3",
+    " bcsonalamo",
 }
 
 
